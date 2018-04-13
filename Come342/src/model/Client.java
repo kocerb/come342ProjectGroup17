d9c6803790a83ecb4c408087bba42c61d4f96e4b
seckincinar <seckincinar@gmail.com>
2018-04-13 16:57:31 +0300

    naps

--- Come342/src/model/Client.java
+++ Come342/src/model/Client.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model;
 
-/**
- *
- * @author burakkocer
- */
-public class Client {
+    import java.util.ArrayList; 
+    import java.util.List; 
+    package javaapplication1; 
+    public class Client { 
+        private String companyName; 
+        private String companyAddress; 
+        private String contactName; 
+        private List<Campaign> CampaignList = new ArrayList<Campaign>(); 
+        public Client(){} 
+        public List<Campaign> getClientCampaign(){ 
+            return CampaignList; 
+        } 
+        public void setcompanyName(String cname){ 
+            companyName=cname; 
+        } 
+        public void setcompanyAddress(String cadd){ 
+            companyAddress=cadd; 
+        } 
+        public void setcontactName(String conName){ 
+            contactName=conName; 
+        } 
+        public String getContactName(){ 
+            return contactName; 
+        } 
+        public String getcompanyName(){
+            return companyName;
+        } 
+        public String getcompanyAddress(){ return companyAddress; } 
+    }
     
 }
