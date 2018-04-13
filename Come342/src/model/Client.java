7ac2a58ee24290107104b40dedfe392dc65f8edc
Burak <burak.kocer@me.com>
2018-04-13 17:44:15 +0300

    Client class modified.

--- Come342/src/model/Client.java
+++ Come342/src/model/Client.java
@@ -1,32 +1,35 @@
+package model;
 
-    import java.util.ArrayList; 
-    import java.util.List; 
-    package javaapplication1; 
-    public class Client { 
-        private String companyName; 
-        private String companyAddress; 
-        private String contactName; 
-        private List<Campaign> CampaignList = new ArrayList<Campaign>(); 
-        public Client(){} 
-        public List<Campaign> getClientCampaign(){ 
-            return CampaignList; 
-        } 
-        public void setcompanyName(String cname){ 
-            companyName=cname; 
-        } 
-        public void setcompanyAddress(String cadd){ 
-            companyAddress=cadd; 
-        } 
-        public void setcontactName(String conName){ 
-            contactName=conName; 
-        } 
-        public String getContactName(){ 
-            return contactName; 
-        } 
-        public String getcompanyName(){
-            return companyName;
-        } 
-        public String getcompanyAddress(){ return companyAddress; } 
-    }
+import java.util.ArrayList; 
+import java.util.List;
     
-}
+public class Client { 
+    private String companyName; 
+    private String companyAddress; 
+    private String contactName; 
+    private List<Campaign> CampaignList = new ArrayList<Campaign>();
+        
+    public Client(){}
+       
+    public List<Campaign> getClientCampaign(){ 
+        return CampaignList; 
+    } 
+    public void setcompanyName(String cname){ 
+        companyName=cname; 
+    } 
+    public void setcompanyAddress(String cadd){ 
+        companyAddress=cadd; 
+    } 
+    public void setcontactName(String conName){ 
+        contactName=conName; 
+    } 
+    public String getContactName(){ 
+        return contactName; 
+    } 
+    public String getcompanyName(){
+        return companyName;
+    } 
+    public String getcompanyAddress(){
+        return companyAddress; 
+    } 
+}    
