8fd70dfcd5fdbc9de97d868537b7a1002b57eab9
Burak <burak.kocer@me.com>
2018-04-13 16:46:29 +0300

    Helper package added.

---
+++ Come342/src/helper/Utils.java
@@ -0,0 +1,16 @@
+package helper;
+
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class Utils {
+    
+    public static void print(String message) {
+        System.out.print(message);
+    }
+    
+    public static void println(String message) {
+        System.out.println(message);
+    }
+}
