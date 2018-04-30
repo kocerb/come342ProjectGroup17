8dc5ac6bb186816020fbbc87d7e17cb4f9db3b2f
Burak <burak.kocer@me.com>
2018-05-01 02:24:16 +0300

    Staff grade database created.

--- Come342/src/come342/Database.java
+++ Come342/src/come342/Database.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import model.Campaign;
 import model.Client;
+import model.StaffMember;
+import model.Grade;
 
 public class Database {
     public static List<Client> clientList = new ArrayList<Client>();
-    public static List<Campaign> campaignList = new ArrayList<Campaign>();
+    public static List<Campaign> campaignList = new ArrayList<Campaign>();    
+    public static List<StaffMember> staffList = new ArrayList<StaffMember>();    
+    public static List<Grade> gradeList = new ArrayList<Grade>();
 
     public static void init() {
         Client eti = new Client("ETI HOLDING", "İstanbul/Gebze", "Ahmet Bey");
--- Come342/src/model/StaffMember.java
+++ Come342/src/model/StaffMember.java
@@ -1,19 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
-/**
- *
- * @author burakkocer
- */
+import model.Grade;
+import java.util.ArrayList;
+import java.util.List;
+import come342.Database;
+
 abstract public class StaffMember {
     private int staffNo;
     private String staffName;
     private String staffStartDate;
     private String staffEmail;
+    private List<Grade> grades = new ArrayList<Grade>();
     
     public StaffMember() {
 
@@ -41,6 +39,11 @@ abstract public class StaffMember {
         }
         public String getStaffEmail(){
             return this.staffEmail;
+        }
+        
+        public void addNewGrade(Grade grade) {
+            this.grades.add(grade);
+            Database.gradeList.add(grade);
         }
 
 }
