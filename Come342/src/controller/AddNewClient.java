f86dce57af9a69042d43574369792dfd9a739d54
pinarbasli <36920703+pinarbasli@users.noreply.github.com>
2018-05-01 16:20:40 +0300

    Update AddNewClient.java

--- Come342/src/controller/AddNewClient.java
+++ Come342/src/controller/AddNewClient.java
@@ -16,7 +16,41 @@ public class AddNewClient {
     }
     
     public void init (){
-
+        addClient();
     }
     
+    public void addClient() {
+        Utils.println("Enter client details");
+		
+		Utils.print("Company Name: ");
+		String name = Utils.scanString();
+		Utils.print("\n");
+
+		Utils.print("Company Address: ");
+		String address = Utils.scanString();
+		Utils.print("\n");
+
+		Utils.print("Company E-mail: ");
+		String email = Utils.scanString();
+		Utils.print("\n");		
+
+		Utils.print("Contact Name: ");
+		String conName = Utils.scanDouble();
+		Utils.print("\n");
+        
+       	Utils.print("Contact E-mail: ");
+		String conEmail = Utils.scanString();
+		Utils.print("\n");	
+
+		Client client = new Client(name, address, email, conName, conEmail);
+
+		Database.clientList.add(client);
+
+		Utils.println("Client added succesfully.");
+        this.addCampaign(Client client);
+    }
+    
+    public void addCampaign() {
+        AddNewCampaign.getInstance.init(client);
+    }
 }
