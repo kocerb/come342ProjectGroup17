3202b0e3e1371b2984ead938b97b013e51a652a8
pinarbasli <36920703+pinarbasli@users.noreply.github.com>
2018-05-01 16:22:57 +0300

    Update AddNewClient.java

--- Come342/src/controller/AddNewClient.java
+++ Come342/src/controller/AddNewClient.java
@@ -3,8 +3,6 @@ package controller;
 import helper.Utils;
 import come342.Database;
 import model.Client;
-//import model.Campaign;
-
 
 public class AddNewClient {
     private static final AddNewClient instance = new AddNewClient();
@@ -22,31 +20,31 @@ public class AddNewClient {
     public void addClient() {
         Utils.println("Enter client details");
 		
-		Utils.print("Company Name: ");
-		String name = Utils.scanString();
-		Utils.print("\n");
+	Utils.print("Company Name: ");
+	String name = Utils.scanString();
+	Utils.print("\n");
 
-		Utils.print("Company Address: ");
-		String address = Utils.scanString();
-		Utils.print("\n");
+	Utils.print("Company Address: ");
+	String address = Utils.scanString();
+	Utils.print("\n");
 
-		Utils.print("Company E-mail: ");
-		String email = Utils.scanString();
-		Utils.print("\n");		
+	Utils.print("Company E-mail: ");
+	String email = Utils.scanString();
+	Utils.print("\n");		
 
-		Utils.print("Contact Name: ");
-		String conName = Utils.scanDouble();
-		Utils.print("\n");
+	Utils.print("Contact Name: ");
+	String conName = Utils.scanDouble();
+	Utils.print("\n");
         
        	Utils.print("Contact E-mail: ");
-		String conEmail = Utils.scanString();
-		Utils.print("\n");	
+	String conEmail = Utils.scanString();
+	Utils.print("\n");	
 
-		Client client = new Client(name, address, email, conName, conEmail);
+	Client client = new Client(name, address, email, conName, conEmail);
 
-		Database.clientList.add(client);
+	Database.clientList.add(client);
 
-		Utils.println("Client added succesfully.");
+	Utils.println("Client added succesfully.");
         this.addCampaign(Client client);
     }
     
